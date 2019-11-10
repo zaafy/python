b917d92c75e407ae1244020f0c8b0195defe1e7b
Daniel Okon <dan.okon77@gmail.com>
2019-11-10 11:19:16 +0100

    add first excercise to python repo

---
+++ exc1.py
@@ -0,0 +1,17 @@
+def getdata() :
+  list = []
+  numberOfItems = int(input("How many items will be in list?\n"))
+  for i in range(0, numberOfItems):
+    list.append(input("Insert data: "))    
+  return list
+
+def listReverse(list) :
+  reversedList = []
+  for i in range(len(list), 0, -1) :
+    reversedList.append(list[i])
+
+  return reversedList
+
+list = []
+list = getdata()
+print(listReverse(list))
